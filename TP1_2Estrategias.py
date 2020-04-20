723fb720a907919d02b4b5739f8f19d7a6162566
NicolasBologna <nicolasandresbologna@hotmail.com>
2020-04-20 02:00:17 -0300

    Added TP 1-2

---
+++ TP1_2Estrategias.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import random as ran
+import numpy as np  
+from scipy import stats
+
+min = 0
+max = 37
+cantidadTiradas =  1000
+ruleta = [] 
+
+def CrearRuleta():
+    ruleta.extend(range(min,max))
+    print("La ruleta es la siguiente:", ruleta)
+
+def GirarRuleta():
+    return ran.randint(min,max-1)
+
+def RealizarTiradas(cantidadTiradas):
+    tiradas = []
+    for i in range(0,cantidadTiradas):
+        tiradas.append(GirarRuleta())
+    return tiradas
